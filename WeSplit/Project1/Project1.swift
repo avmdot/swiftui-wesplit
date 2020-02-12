c33c4c66970730f5300331498bd129f5d58f7f69
Alejandro Vicente <alejandro.meteored@gmail.com>
2020-02-12 11:27:21 +0100

    defining project

---
+++ WeSplit/Project1/Project1.swift
@@ -0,0 +1,67 @@
+//
+//  Project1.swift
+//  WeSplit
+//
+//  Created by Alejandro Vicente Milán on 12/02/2020.
+//  Copyright © 2020 Alejandro Vicente Milán. All rights reserved.
+//
+
+import SwiftUI
+
+struct Project1: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 0
+    
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let orderAmount = Double(checkAmount) ?? 0
+        let peopleCount = Double(numberOfPeople + 1)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(1 ..< 11) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("$\(totalPerPerson)")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
+    }
+}
+
+struct Project1_Previews: PreviewProvider {
+    static var previews: some View {
+        Project1()
+    }
+}
