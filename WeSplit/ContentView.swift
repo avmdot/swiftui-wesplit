5098ed89e275eef8116506c92941f2fa45747db8
Alejandro Vicente <alejandro.meteored@gmail.com>
2020-02-12 08:54:25 +0100

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Alejandro Vicente Milán on 12/02/2020.
+//  Copyright © 2020 Alejandro Vicente Milán. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
